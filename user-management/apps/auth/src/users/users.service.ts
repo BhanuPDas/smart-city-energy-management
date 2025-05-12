5b25baa83b61eebdc49f57605052ca5388ff9965
CodingF0X <roody9a@gmail.com>
2025-05-12 18:49:40 +0200

    -- hashed the password upon user registeration

--- user-management/apps/auth/src/users/users.service.ts
+++ user-management/apps/auth/src/users/users.service.ts
@@ -8,14 +8,23 @@ import { User } from './entities/user.entity';
 import { CreateUserDTO } from './DTO/create-user.dto';
 import { Long, QueryFailedError } from 'typeorm';
 import { UpdateUserDTO } from './DTO/update-user.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly userRepository: UsersRepository) {}
 
+    async hashPassword(password: string): Promise<string> {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    return hashedPassword;
+  }
+
   async createUser(createUserDto: CreateUserDTO): Promise<User> {
     try {
-      return await this.userRepository.create(createUserDto);
+      return await this.userRepository.create({
+        ...createUserDto,
+        password: await this.hashPassword(createUserDto.password),
+      });
     } catch (err) {
       if (err instanceof QueryFailedError && (err as any).code === '23505') {
         throw new ConflictException('Email is already in use');
