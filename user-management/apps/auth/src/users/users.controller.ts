f5e7b36dfec676da5e9adf8e150ab26546bac8f0
CodingF0X <roody9a@gmail.com>
2025-05-15 11:03:45 +0200

    -- refactor- protected the routes

--- user-management/apps/auth/src/users/users.controller.ts
+++ user-management/apps/auth/src/users/users.controller.ts
@@ -15,7 +15,16 @@ import { Long } from 'typeorm';
 import { UpdateUserDTO } from './DTO/update-user.dto';
 import { JwtAuthGuard } from '../guards/jwt.-auth.guard';
 import { Roles, RolesGuard } from '@app/common/auth';
-import { Public, Role, SwaggerCreateUSER, SwaggerDeleteUser, SwaggerGetAllUsers, SwaggerGetUserByEmail, SwaggerGetUserById, SwaggerUpdateUser } from '@app/common';
+import {
+  Public,
+  Role,
+  SwaggerCreateUSER,
+  SwaggerDeleteUser,
+  SwaggerGetAllUsers,
+  SwaggerGetUserByEmail,
+  SwaggerGetUserById,
+  SwaggerUpdateUser,
+} from '@app/common';
 
 @Controller({ path: 'users', version: '1' })
 export class UsersController {
@@ -30,25 +39,31 @@ export class UsersController {
 
   @Get()
   @UseGuards(JwtAuthGuard, RolesGuard)
-  @Roles(Role.CITIZEN)
+  @Roles(Role.CITY_PLANNER)
   @SwaggerGetAllUsers(User)
   async findAll(): Promise<User[]> {
     return await this.usersService.findAll();
   }
 
   @Get('/email/:email')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.CITY_PLANNER)
   @SwaggerGetUserByEmail(User)
   async findOne(@Param('email') email: string): Promise<User> {
     return await this.usersService.findOne(email);
   }
 
   @Get('/id/:id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.CITY_PLANNER)
   @SwaggerGetUserById(User)
   async findOneById(@Param('id') id: Long): Promise<User> {
     return await this.usersService.findOneById(id);
   }
 
   @Put('/id/:id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.CITY_PLANNER)
   @SwaggerUpdateUser(User)
   async findOneAndUpdate(
     @Param('id') id: Long,
@@ -58,6 +73,8 @@ export class UsersController {
   }
 
   @Delete('/id/:id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.CITY_PLANNER)
   @SwaggerDeleteUser(User)
   async findOneAndDelete(@Param('id') id: Long): Promise<string> {
     return await this.usersService.findOneAndDelete(id);
