c43149b385e25e5bf916cf37a773b83ba5f223e5
BhanuPDas <bhanu.das001@stud.fh-dortmund.de>
2025-05-17 09:50:09 +0200

    Adding changes for city management service

---
+++ city-management-service/src/main/java/de/fhdo/city_mgmt_service/config/CityMgmtConfig.java
@@ -0,0 +1,31 @@
+package de.fhdo.city_mgmt_service.config;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+@Configuration
+public class CityMgmtConfig {
+
+	@Bean(autowireCandidate = true)
+	@Scope("singleton")
+	ObjectMapper createObjectMapper() {
+		ObjectMapper obj = new ObjectMapper();
+		obj.registerModule(new JavaTimeModule());
+		return obj;
+	}
+
+	@Bean(autowireCandidate = true)
+	@Scope("singleton")
+	@LoadBalanced
+	RestTemplate createRestTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+}
