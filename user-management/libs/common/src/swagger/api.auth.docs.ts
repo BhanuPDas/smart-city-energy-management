c0f7d2f29ddb2c300eb26ad671e9af961f767ba2
CodingF0X <roody9a@gmail.com>
2025-05-19 10:16:32 +0200

    -- Updated the APi-docs

--- user-management/libs/common/src/swagger/api.auth.docs.ts
+++ user-management/libs/common/src/swagger/api.auth.docs.ts
@@ -10,10 +10,35 @@ export function SwaggerLogin<TModel extends Type<any>>(
     ApiOperation({ summary }),
     ApiResponse({
       status: 201,
-      description: `${model.name} Logged in successfully`,
+      description: `success`,
       type: model,
+      example: {
+        access_token:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI0IiwiZW1haWwiOiJtNWVlZkBnbWFpbC5jb20iLCJyb2xlIjoiY2l0eV9wbGFubmVyIiwiaWF0IjoxNzQ3NjAwMTIxLCJleHAiOjE3NDc2MDEwMjF9.lxoVC_kBQWWXPM8ogHqBO72QY68llhy_sz1OIaVi9wQ',
+        user: {
+          userId: 1,
+          name: 'John Doe',
+          email: 'John_Doe@gmail.com',
+          phone: '124',
+          role: 'city_planner',
+        },
+      },
     }),
-    ApiResponse({ status: 400, description: 'Bad Request' }),
+    ApiResponse({
+      status: 404,
+      description: 'NotFoundException',
+      example: {
+        response: {
+          message: 'Entity Not Found',
+          error: 'Not Found',
+          statusCode: 404,
+        },
+        status: 404,
+        options: {},
+        message: 'Entity Not Found',
+        name: 'NotFoundException',
+      },
+    }),
     ApiBody({
       schema: {
         type: 'object',
@@ -23,10 +48,9 @@ export function SwaggerLogin<TModel extends Type<any>>(
         },
         required: ['email', 'password'],
       },
-    })
+    }),
   );
 }
-
 
 export function SwaggerLogout<TModel extends Type<any>>(
   model: TModel,
@@ -51,9 +75,20 @@ export function SwaggerVerifyUser<TModel extends Type<any>>(
     ApiOperation({ summary }),
     ApiResponse({
       status: 201,
-      description: `${model.name} User verified successfully`,
+      description: `success`,
       type: model,
+      example: {
+        status: 'alive',
+      },
     }),
-    ApiResponse({ status: 400, description: 'Bad Request' }),
+
+    ApiResponse({
+      status: 401,
+      description: 'Unauthorized',
+      type: model,
+      example: {
+        status: 'expired',
+      },
+    }),
   );
 }
