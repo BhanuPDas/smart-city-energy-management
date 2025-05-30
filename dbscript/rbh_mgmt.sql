eb1d22fc6c8dd7faa816427f590de92e3103499c
Hadis Delbord <hadisdelbord@gmail.com>
2025-05-30 20:42:07 +0200

    Updating dbscript
    # Conflicts:
    #	.gitignore

--- dbscript/rbh_mgmt.sql
+++ dbscript/rbh_mgmt.sql
@@ -1,30 +1,56 @@
 CREATE SCHEMA IF NOT EXISTS rbh_mgmt;
+CREATE TABLE IF NOT EXISTS rbh_mgmt.energy_type
+(
+    price_per_unit integer,
+    id bigint NOT NULL,
+    name character varying(255) COLLATE pg_catalog."default",
+    provider character varying(255) COLLATE pg_catalog."default",
+    unit character varying(255) COLLATE pg_catalog."default",
+    CONSTRAINT energy_type_pkey PRIMARY KEY (id)
+);
+INSERT INTO public.energy_type(
+    price_per_unit, id, name, provider, unit)
+VALUES (100, 1, 'solar', 'E-ON', 'm3');
+
+INSERT INTO public.energy_type(
+    price_per_unit, id, name, provider, unit)
+VALUES (200, 2, 'electricity', 'RWE', 'kwh');
+
+INSERT INTO public.energy_type(
+    price_per_unit, id, name, provider, unit)
+VALUES (300, 3, 'gas', 'DEW21', 'm3');
+
+INSERT INTO public.energy_type(
+    price_per_unit, id, name, provider, unit)
+VALUES (400, 4, 'heating-pump', 'octapus', 'm3');
+
+
+CREATE TABLE IF NOT EXISTS rbh_mgmt.building
+(
+    floor_area integer,
+    zip_code integer,
+    id bigint NOT NULL,
+    address character varying(255) COLLATE pg_catalog."default",
+    city character varying(255) COLLATE pg_catalog."default",
+    owner_email character varying(255) COLLATE pg_catalog."default",
+    CONSTRAINT building_pkey PRIMARY KEY (id)
+);
 CREATE TABLE IF NOT EXISTS rbh_mgmt.energy_source
 (
-    energy_type VARCHAR(14) PRIMARY KEY,
-    provider VARCHAR(15) NOT NULL,
-    price_per_unit real NOT NULL
-);
-INSERT INTO rbh_mgmt.energy_source(energy_type, provider, price_per_unit) VALUES ('renewable', 'DEW21', 0.25);
-INSERT INTO rbh_mgmt.energy_source(energy_type, provider, price_per_unit) VALUES ('solar', 'DEW21', 0.30);
-INSERT INTO rbh_mgmt.energy_source(energy_type, provider, price_per_unit) VALUES ('electricity', 'DEW21', 0.50);
-INSERT INTO rbh_mgmt.energy_source(energy_type, provider, price_per_unit) VALUES ('gas', 'DEW21', 0.65);
-CREATE TABLE IF NOT EXISTS rbh_mgmt.building_details
-(
-    id SERIAL PRIMARY KEY,
-    address VARCHAR(50) NOT NULL,
-    zipcode integer NOT NULL,
-    city VARCHAR(30) NOT NULL,
-    owner_email VARCHAR(35)
-);
-CREATE TABLE IF NOT EXISTS rbh_mgmt.energy_details
-(
-    id SERIAL PRIMARY KEY ,
-    building_id integer NOT NULL,
-    energy_type character varying(14),
-    start_date date,
-    end_date date,
-    units_consumed integer,
-    amount real
+    consumption integer,
+    building_id bigint NOT NULL,
+    end_date timestamp(6) without time zone,
+    energy_type_id bigint NOT NULL,
+    id bigint NOT NULL,
+    start_date timestamp(6) without time zone,
+    CONSTRAINT energy_source_pkey PRIMARY KEY (id),
+    CONSTRAINT fkclt7a13bgd1o8si3oagl5u4ky FOREIGN KEY (building_id)
+    REFERENCES public.building (id) MATCH SIMPLE
+                          ON UPDATE NO ACTION
+                          ON DELETE NO ACTION,
+    CONSTRAINT fkqcdjkhvyi6s26dpjqoknctk2x FOREIGN KEY (energy_type_id)
+    REFERENCES public.energy_type (id) MATCH SIMPLE
+                          ON UPDATE NO ACTION
+                          ON DELETE NO ACTION
 );
 COMMIT;
