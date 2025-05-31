03f26a5ab2e0a780ff4d46a8c8a7d53f005de12f
Amir Hossein Pakdel <amir.pakdel1378@yahoo.com>
2025-05-31 22:17:56 +0200

    Make Energy Analysis Service

---
+++ energy_analysis_service/src/main/java/com/fhdo/energy_analysis_service/EnergyAnalysisServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fhdo.energy_analysis_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyAnalysisServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnergyAnalysisServiceApplication.class, args);
+    }
+
+}
---
+++ energy_analysis_service/src/test/java/com/fhdo/energy_analysis_service/EnergyAnalysisServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fhdo.energy_analysis_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyAnalysisServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
