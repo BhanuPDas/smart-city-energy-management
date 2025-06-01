be974df560a95f77d7c3b084d68b70c83268f6b8
Bhanu Pratap Das <bhanupd199@gmail.com>
2025-06-01 14:53:08 +0200

    Refactoring analytics

--- energy_analytics_service/src/main/java/com/fhdo/energy_analytics_service/domain/entity/BuildingEntity.java
+++ energy_analytics_service/src/main/java/com/fhdo/energy_analytics_service/domain/entity/BuildingEntity.java
@@ -12,16 +12,12 @@ public class BuildingEntity {
 
     @Id
     private Long id;
-
     @Column(name = "owner_email")
     private String ownerEmail;
-
     private String address;
     private String city;
-
     @Column(name = "zip_code")
     private Integer zipCode;
-
     @Column(name = "floor_area")
     private Integer floorArea;
 }
